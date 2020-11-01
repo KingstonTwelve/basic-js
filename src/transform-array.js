4fb53edf077e8dc493b3b060b8a7e426ae48d404
Maxim Vinnikov <maxim.vinnikoff@gmail.com>
2020-11-01 06:18:30 +0300

    feat: implement task transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,42 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error();
+  }
+  if (!arr.length) {
+    return arr;
+  }
+  let transformArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case '--discard-next':
+        if (i < arr.length - 1) {
+          i++;
+        }
+        break;
+
+      case '--discard-prev':
+        if (i >= 1 && (arr[i - 2] != '--discard-next')) {
+          transformArr.splice(-1, 1);
+        }
+        break;
+
+      case '--double-next':
+        if (i < arr.length - 1) {
+          transformArr.push(arr[i + 1]);
+        }
+        break;
+
+      case '--double-prev':
+        if (i >= 1 && (arr[i - 2] != '--discard-next')) {
+          transformArr.push(arr[i - 1]);
+        }
+        break;
+
+      default:
+        transformArr.push(arr[i]);
+    }
+  }
+  return transformArr;
 };
