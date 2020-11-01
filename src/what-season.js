687d0e85c494b71c95071362166e87bd23e762af
Maxim Vinnikov <maxim.vinnikoff@gmail.com>
2020-11-01 04:31:19 +0300

    feat: implement task what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,22 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (!date) {
+    return 'Unable to determine the time of year!';
+  }
+  const month = date.getMonth();
+  if (typeof date === 'object' && !Object.keys(date).length){
+    if (month === 0 || month === 1 || month === 11) {
+      return 'winter';
+    } else if (month === 2 || month === 3 || month === 4) {
+      return 'spring';
+    } else if (month === 5 || month === 6 || month === 7) {
+      return 'summer';
+    } else if (month === 8 || month === 9 || month === 10) {
+      return 'fall';
+    }
+  } else {
+    throw new Error();
+  }
+  
 };
